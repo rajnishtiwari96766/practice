597bcc7cb2c15a10dfad56b18b27df8d0df8f9f6
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-10-10 20:17:53 +0530

    new commit

--- src/card.js
+++ src/card.js
@@ -2,14 +2,24 @@ import React from 'react'
 import details from './data'
 
 const Card = () => {
+  const handleClick=()=>{
+    window.open('https://www.netflix.com/watch/80205342?trackId=255824129')
+  }
   return (
-    <div>
+   <>
         {
-        details.map((val,index)=>{
-            
-        })
+        details.map((val,index)=>(
+          <div key={index}>
+            <p>{val.name}</p>
+            <img src={val.image}></img>
+            {/* <a href={val.link}><button>click me</button></a> */}
+            <button onClick={handleClick}>Click me</button>
+          </div>
+        )
+        )
         }
-    </div>
+   </>
+   
   )
 }
 
--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
 const details=[
     {
         name:'Money heist',
-        image:'https://m.media-amazon.com/images/M/MV5BODI0ZTljYTMtODQ1NC00NmI0LTk1YWUtN2FlNDM1MDExMDlhXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_FMjpg_UX1000_.jpg',
+        image:"https://w0.peakpx.com/wallpaper/547/1017/HD-wallpaper-money-heist-ei-professor-profile-whatsapp-dp.jpg",
         link:'https://www.netflix.com/watch/80205342?trackId=255824129'
     }
 ]
-
 export default details;
