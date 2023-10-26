98def0877bbca3ca616eb7bd63c253df866f74b1
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-10-26 23:50:14 +0530

    new commit

--- src/logic.js
+++ src/logic.js
@@ -2,19 +2,31 @@ import React, { useState } from 'react'
 
 const Logic = () => {
  const [value,setvalue]=useState('');
+ const [message, setmessage] = useState([])
 
  const handleChange=(event)=>{
-  setvalue(event.target.value); //this will set the final value as in the form of value
- }
 
- const handleClick=(event)=>{
-   <p>{value}</p>
- }
+  setvalue(event.target.value); //this will set the final value as in the form of value
+}
+
+const handleClick=()=>{
+  setmessage(value)
+}
+
   return (
     <div>
       <h1>Todo List</h1>
       <input type='text' placeholder='Enter the text' onChange={handleChange}/>
       <button onClick={handleClick}>➕</button>
+     
+      <div>
+      {/* <p>{message}</p> */}
+      {message.map((value)=>{
+        return(
+          <p>{message}</p>
+        )
+      })}
+      </div>
     </div>
   )
 }
