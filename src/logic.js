b27430e24cacd2b1f32453456ec790642c5d8d35
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-10-28 13:03:58 +0530

    new commit

--- src/logic.js
+++ src/logic.js
@@ -7,32 +7,31 @@ const Logic = () => {
  const handleChange=(event)=>{
 
   setvalue(event.target.value); //this will set the final value as in the form of value
-  
 }
 
-const addValue=()=>{
+const addValue=(event)=>{
   setmessage([...message,value]) //here the value is appended
-  setvalue(' ')
+  // event.preventDefault()
+  setvalue('')
 }
 
 const removeValue=(index)=>{
   const newMsg=[...message]
   newMsg.splice(index,1)
   setvalue(newMsg)
-
 }
 
   return (
     <div>
       <h1>Todo List</h1>
-      <input type='text' placeholder='Enter the text' onChange={handleChange}/>
+      <input type='text' placeholder='Enter the text' value={value} onChange={handleChange}/>
       <button onClick={addValue}>➕</button>
      
       <div>
       {message.map((val,index)=>{
         return(
           <ul>
-            <li>{val} <button onClick={removeValue}> ➖</button></li>
+            <li key={index}>{val}</li>
           </ul>
         )
       })}
